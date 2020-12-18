5187d77f65ae2d20bc047c5119a71f55d12129fd
Arthur Rabelo <arthur.04.pb@gmail.com>
2020-12-18 09:43:45 -0300

    Atualizando aquivo de conexão com MongoDB

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-export default mongoose.connect("mongodb+srv://admin_todoApp:G4p5h8G7yb8BukJu@cluster0.onij3.mongodb.net/TodoApp", {
+export default mongoose.connect("mongodb+srv://<username>:<dbpassword>@cluster0.onij3.mongodb.net/<dbname>", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: true
